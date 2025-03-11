76f884db5eed87c433d780d5e1f4823e9cf5ecdb
Maks Mykolenko <72696352+MaksMykolenko@users.noreply.github.com>
2025-03-11 14:24:47 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const mobileMenu = document.getElementById("mobile-menu");
-    const navList = document.querySelector(".nav-list");
+    const menuToggle = document.getElementById("menu-toggle");
+    const navMenu = document.getElementById("nav-menu");
 
-    mobileMenu.addEventListener("click", function () {
-        navList.classList.toggle("active");
+    menuToggle.addEventListener("click", function () {
+        navMenu.classList.toggle("active"); 
+        menuToggle.classList.toggle("active"); 
+    });
+
+    // Закриття меню при натисканні на посилання
+    document.querySelectorAll(".navbar ul li a").forEach(item => {
+        item.addEventListener("click", () => {
+            navMenu.classList.remove("active");
+            menuToggle.classList.remove("active");
+        });
     });
 });
